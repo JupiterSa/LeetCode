1c9c7e435a2d4bf599ef00a18e1f60e9cfbc650b
He <11@qq.com>
2020-07-11 14:11:46 +0800

    JJ

---
+++ 203-Remove-Linked-List-Elements/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //自己测试代码，需要调用solution中的removeElements(ListNode head, int val)
+        //所以我们自己构造出 ListNode head！！！
+        int[] nums = {1,2,6,3,4,5,6};
+
+
+
+
+    }
+}
---
+++ 203-Remove-Linked-List-Elements/src/Solution.java
@@ -0,0 +1,72 @@
+/**
+ * 203. 移除链表元素
+ *
+ * 难度：简单
+ *
+ * 题目：删除链表中等于给定值 val 的所有节点。
+ *
+ * 示例:
+ * 输入: 1->2->6->3->4->5->6, val = 6
+ * 输出: 1->2->3->4->5
+ *
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    //解法一，使用dummyHead
+    public ListNode removeElements(ListNode head, int val) {
+        //方法一 循环链表，并设置dummyHead，如果改节点等于val，就将dummyHead指向下一个节点
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;  //让prev跨国被del节点，prev已经相当于移动了
+                delNode.next = null; //让被删除节点与链表断掉联系
+            } else {
+                prev = prev.next; //单纯移动pre向后一位  //这句话要用else包起来，不能直接放在if后面，因为if后面，因为上门已经移动了
+            }
+        }
+        return dummyHead.next; //这里要返回要注意，因为有可能除了dummyHead，其他都被删除了
+    }
+
+    //解法二  直接使用head，需要在代码里对head做特殊处理
+    public ListNode removeElements2(ListNode head, int val) {
+        //先处理head 首先head不为空，且head==val，则删除head
+//        if ( head.val == val && head != null) {
+//            head = head.next;
+//        }
+
+        //!!!! 上面的if中的逻辑是循环的，所以把if改成while！！！！！
+        while ( head.val == val && head != null) {
+            head = head.next;  //这个方法是简单写，因为leetcode里提交忽略了loitering object
+        }
+
+        //一路删下去，head都是val，在这里head约等于一个current指针
+        if (head == null)
+            return null;
+
+        //有head，且head不等于val，head可以看作prev，然后一路判断下去
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;  //让prev跨国被del节点，prev已经相当于移动了
+                delNode.next = null; //让被删除节点与链表断掉联系
+            } else {
+                prev = prev.next; //单纯移动pre向后一位  //这句话要用else包起来，不能直接放在if后面，因为if后面，因为上门已经移动了
+            }
+        }
+        return head;
+    }
+}
